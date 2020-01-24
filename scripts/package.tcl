eebb016e1b4dec5e477686d9ea08188980832eca
Oliver Bründleer <oliver.bruendler@enclustra.com>
2020-01-24 09:36:46 +0100

    DEVEL: Packaged new version

--- scripts/package.tcl
+++ scripts/package.tcl
@@ -8,7 +8,7 @@ namespace import -force psi::ip_package::latest::*
 # General Information
 ###############################################################
 set IP_NAME fpga_base
-set IP_VERSION 1.3
+set IP_VERSION 1.4
 set IP_REVISION "auto"
 set IP_LIBRARY PSI
 set IP_DESCIRPTION "FPGA Version information (SW and FW build date)"
--- xgui/fpga_base_v1_4.tcl
+++ xgui/fpga_base_v1_4.tcl
@@ -6,6 +6,7 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "C_VERSION" -parent ${Configuration}
   ipgui::add_param $IPINST -name "C_VERSION_MAJOR" -parent ${Configuration}
   ipgui::add_param $IPINST -name "C_VERSION_MINOR" -parent ${Configuration}
+  ipgui::add_param $IPINST -name "C_USE_INFO_FROM_SCRIPT" -parent ${Configuration}
 
 
 }
@@ -16,6 +17,15 @@ proc update_PARAM_VALUE.C_S00_AXI_ID_WIDTH { PARAM_VALUE.C_S00_AXI_ID_WIDTH } {
 
 proc validate_PARAM_VALUE.C_S00_AXI_ID_WIDTH { PARAM_VALUE.C_S00_AXI_ID_WIDTH } {
 	# Procedure called to validate C_S00_AXI_ID_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_USE_INFO_FROM_SCRIPT { PARAM_VALUE.C_USE_INFO_FROM_SCRIPT } {
+	# Procedure called to update C_USE_INFO_FROM_SCRIPT when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_USE_INFO_FROM_SCRIPT { PARAM_VALUE.C_USE_INFO_FROM_SCRIPT } {
+	# Procedure called to validate C_USE_INFO_FROM_SCRIPT
 	return true
 }
 
@@ -60,6 +70,11 @@ proc update_MODELPARAM_VALUE.C_VERSION_MAJOR { MODELPARAM_VALUE.C_VERSION_MAJOR 
 proc update_MODELPARAM_VALUE.C_VERSION_MINOR { MODELPARAM_VALUE.C_VERSION_MINOR PARAM_VALUE.C_VERSION_MINOR } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.C_VERSION_MINOR}] ${MODELPARAM_VALUE.C_VERSION_MINOR}
+}
+
+proc update_MODELPARAM_VALUE.C_USE_INFO_FROM_SCRIPT { MODELPARAM_VALUE.C_USE_INFO_FROM_SCRIPT PARAM_VALUE.C_USE_INFO_FROM_SCRIPT } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_USE_INFO_FROM_SCRIPT}] ${MODELPARAM_VALUE.C_USE_INFO_FROM_SCRIPT}
 }
 
 proc update_MODELPARAM_VALUE.C_S00_AXI_ID_WIDTH { MODELPARAM_VALUE.C_S00_AXI_ID_WIDTH PARAM_VALUE.C_S00_AXI_ID_WIDTH } {
